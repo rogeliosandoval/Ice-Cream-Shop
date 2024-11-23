9b795b65049f874b835d1246ea5209c15a7998ea
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-11-22 18:13:27 -0600

    Add & Delete

--- src/app/interfaces/menu.interfaces.ts
+++ src/app/interfaces/menu.interfaces.ts
@@ -1,5 +1,5 @@
 export interface MenuItem {
-  id: number
+  id?: string
   name: string
   price: number 
   calories: number
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MenuItem } from '../../interfaces/menu.interfaces';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { DevkitFileSystem } from '@angular/cdk/schematics';
 
 @Component({
   selector: 'app-menu',
@@ -17,9 +18,10 @@ import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } 
 export class MenuComponent implements OnInit {
   private http = inject(HttpClient)
   private api = 'https://672ec227229a881691f0d9bf.mockapi.io/scoop/ice-creams'
+  public showEditForm = signal<boolean>(false)
   // public iceCreams = signal<MenuItem[]>([])
   public iceCreams = signal<MenuItem[]>([{
-    id: 0,
+    id: '',
     name: '',
     price: 0,
     calories: 0,
@@ -31,16 +33,25 @@ export class MenuComponent implements OnInit {
   
   public iceCreamForm = new FormGroup({
     name: new FormControl(''),
-    price: new FormControl(null),
-    calories: new FormControl(null),
+    price: new FormControl(''),
+    calories: new FormControl(''),
     category: new FormControl(''),
     description: new FormControl('')
   })
+
+  // public iceCreamFormEdit = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(''),
+  //   calories: new FormControl(''),
+  //   category: new FormControl(''),
+  //   description: new FormControl('')
+  // })
 
   ngOnInit(): void {
     setTimeout(() => {
       this.http.get<MenuItem[]>(this.api).subscribe({
         next: response => {
+          console.log(response)
           this.iceCreams.set(response)
           this.showData.set(true)
         },
@@ -48,21 +59,52 @@ export class MenuComponent implements OnInit {
           this.showError.set(true)
         },
         complete: () => {
-          console.log('The subscription has completed!')
+          // console.log('The subscription has completed!')
         }
       })
     }, 3000)
   }
 
   public addItem(): void {
+    this.http.post<MenuItem>(this.api, this.iceCreamForm.value).subscribe({
+      next: response => {
+        
+      },
+      error: err => {
 
+      },
+      complete: () => {
+        window.location.reload()
+      }
+    })
   }
 
-  public deleteItem(): void {
+  public deleteItem(id: any): void {
+    // `/${id}` this is called template literal, it's for making strings in JavaScript '$' is called intorpolation
+    this.http.delete(this.api + `/${id}`).subscribe({
+      next: response => {
 
+      },
+      error: err => {
+
+      },
+      complete: () => {
+        window.location.reload()
+      }
+    })
   }
 
-  public updateItem(): void {
+  // public updateItem(id: any): void {
+  //   this.http.patch<MenuItem>(this.api + `/${id}`, this.iceCreamFormEdit.value).subscribe({
+  //     next: response => {
+  //       console.log(response)
+  //     },
+  //     error: err => {
 
-  }
+  //     },
+  //     complete: () => {
+
+  //     }
+  //   })
+  // }
 }
